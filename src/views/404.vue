6932b03c401abd1c81f2c975dec2cf36977a80c1
Megan Reiffer <meganreiffer@gmail.com>
2021-07-01 10:35:36 -0400

    updates

--- src/views/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div id="404">
-        <h1 class="display-1" >404</h1>
-        <h2 class="">Page not found</h2>
-        <b-button variant="outline-dark" to="/home">Go Home</b-button>
-    </div>
-</template>
